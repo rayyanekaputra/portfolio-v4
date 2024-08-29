c28bb0996f7b764be10a84831b9c1591992228e5
rayyanekaputra <bullzhit99@gmail.com>
2024-08-30 02:42:41 +0800

    added services animation card

--- src/app/components/home/services/services.jsx
+++ src/app/components/home/services/services.jsx
@@ -1,46 +1,81 @@
-'use client'
+"use client";
 import styles from "./styles.module.css";
-import services_list from "./data/services_obj"
-import Image from "next/image";
-
+import services_list from "./data/services_obj";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Services() {
+  //https://codesandbox.io/p/sandbox/musicapp-with-react-and-gsap-forked-csqtx?file=%2Fsrc%2FTest.js%3A24%2C20-24%2C33
+  //https://gsap.com/community/forums/topic/27012-scrolltrigger-and-array-of-refs-in-react/
+  const bottomRowRef = useRef();
+  useEffect(() => {
+    const listServices = bottomRowRef.current.querySelectorAll(`.${styles.listServices}`);
+    gsap.fromTo(listServices, {
+        opacity: 0
+    },{
+      delay: 6,
+      duration: 2,
+      opacity: 1,
+      stagger: 1,
+      ease: "power1.inOut"
+    });
+  }, []);
 
-    return(
-        <div className={styles.services}>
-            <div className={styles.topRow}>
-                <h2>Services</h2>
-                <div className={styles.seeMore}>
-                    <p className={styles.seeMoreText}>See More</p>
-                    <div className={styles.icon}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-  <path d="M5.33333 14.9993L13.3333 6.99935V14.166H15V4.16602H5V5.83268H12.1667L4.16667 13.8327L5.33333 14.9993Z" fill="black"/>
-</svg>
-                    </div>
-                    
-                </div>
-            </div>
-            <div>
-                <ul className={styles.bottomRow}>
-
-                {services_list.map(({title, description}, index)=>(
-                    <li  key={index} data-scroll data-scroll-speed='0.1' className={styles.listServices}>
-                        <div className={styles.lisServiceItemTop}>
-                         <h2>{title}</h2>
-                         <div className={styles.iconServiceItem}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-  <path d="M5.33333 14.9993L13.3333 6.99935V14.166H15V4.16602H5V5.83268H12.1667L4.16667 13.8327L5.33333 14.9993Z" fill="white"/>
-</svg>
-                    </div>
-                         </div>
-                            
-                        <p>{description}</p>
-                     </li>
-                ))}
-                </ul>
-            </div>
+  return (
+    <div className={styles.services}>
+      <div className={styles.topRow}>
+        <h2>Services</h2>
+        <div className={styles.seeMore}>
+          <p className={styles.seeMoreText}>See More</p>
+          <div className={styles.icon}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+            >
+              <path
+                d="M5.33333 14.9993L13.3333 6.99935V14.166H15V4.16602H5V5.83268H12.1667L4.16667 13.8327L5.33333 14.9993Z"
+                fill="black"
+              />
+            </svg>
+          </div>
         </div>
+      </div>
+      <div>
+        <ul className={styles.bottomRow} ref={bottomRowRef}>
+          {services_list.map(({ title, description }, index) => (
+            <li
+              key={index}
+              data-scroll
+              data-scroll-speed="0.1"
+              className={styles.listServices}
+            >
+              <div className={styles.lisServiceItemTop}>
+                <h2>{title}</h2>
+                <div className={styles.iconServiceItem}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <path
+                      d="M5.33333 14.9993L13.3333 6.99935V14.166H15V4.16602H5V5.83268H12.1667L4.16667 13.8327L5.33333 14.9993Z"
+                      fill="white"
+                    />
+                  </svg>
+                </div>
+              </div>
 
-    )
-
+              <p>{description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
